282d9168001fa50ec842fe4838e84e82b32a4af7
SITIChile <github@SITIChile.cl>
2018-07-03 18:07:54 -0300

    Tamaño etiqueta impresora

--- public/barcode/codigoBarra.php
+++ public/barcode/codigoBarra.php
@@ -70,7 +70,7 @@ for($i=0; $i<$cantidad;$i++){
 	//$pdf->Rotate(-90);
 
 	$pdf->Cell(0, 0, $producto, 0, 1, 'C');
-	$pdf->write1DBarcode($codigo, 'C128A', '', '', '', 13, 0.4, $style, 'N');
+	$pdf->write1DBarcode($codigo, 'UPC-E', '', '', '', 13, 0.4, $style, 'N');
 	$pdf->SetFont('helvetica', '', 10);
 	$pdf->Cell(0, 0, $precio, 0, 1, 'C');
 	$pdf->SetFont('helvetica', '', 6);
